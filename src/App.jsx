6291a7f1e5cc824094899ee0f325f5b670fa44fb
Patrik Rehak <1patrikrehak@gmail.com>
2023-07-31 17:53:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./SearchParams";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  const adoptedPet = useState(null);
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <h1>Adopt Me!</h1>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
